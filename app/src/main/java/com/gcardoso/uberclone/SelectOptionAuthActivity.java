fffb6cfb5ebd968f04fc36fcd2bcf80a339ba0f0
gcardosov <ger9047@gmail.com>
2020-05-06 21:46:35 -0500

    Diseño de pantalla de selecion

---
+++ app/src/main/java/com/gcardoso/uberclone/SelectOptionAuthActivity.java
@@ -0,0 +1,14 @@
+package com.gcardoso.uberclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectOptionAuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_option_auth);
+    }
+}
