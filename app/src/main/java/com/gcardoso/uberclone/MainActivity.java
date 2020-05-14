42cb7bde1da6302be22f7deff920d4bd2d2a06a7
gcardosov <ger9047@gmail.com>
2020-05-13 19:17:54 -0500

    Click listener y navegar entre pantallas

--- app/src/main/java/com/gcardoso/uberclone/MainActivity.java
+++ app/src/main/java/com/gcardoso/uberclone/MainActivity.java
@@ -2,13 +2,48 @@ package com.gcardoso.uberclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mButtonIAmClient;
+    Button mButtonIAmDriver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonIAmClient = findViewById(R.id.btnIAmClient);
+        mButtonIAmDriver = findViewById(R.id.btnIAmDriver);
+
+
+        mButtonIAmClient.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToSelectAuth();
+
+            }
+        });
+
+        mButtonIAmDriver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToSelectAuth();
+
+            }
+        });
+
+
+    }
+
+
+
+    private void goToSelectAuth() {
+        Intent intent = new Intent(MainActivity.this, SelectOptionAuthActivity.class);
+        startActivity(intent);
     }
 }
