9eeb265289b7260548d6803a8f9f7dc12f724d1d
gcardosov <ger9047@gmail.com>
2020-06-24 00:38:36 -0500

    Utilizar el GPS para obtener ubicacion actual

--- app/src/main/java/com/gcardoso/uberclone/activities/driver/MapDriverActivity.java
+++ app/src/main/java/com/gcardoso/uberclone/activities/driver/MapDriverActivity.java
@@ -1,10 +1,19 @@
 package com.gcardoso.uberclone.activities.driver;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -13,9 +22,17 @@ import com.gcardoso.uberclone.activities.MainActivity;
 import com.gcardoso.uberclone.activities.client.MapClientActivity;
 import com.gcardoso.uberclone.includes.MyToolbar;
 import com.gcardoso.uberclone.providers.AuthProvider;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
 //Para implementar la interface OnMapReadyCallback nos marca errores por lo cual necesitamos
 //sobreesribir algunos metodos
 
@@ -25,6 +42,32 @@ public class MapDriverActivity extends AppCompatActivity implements OnMapReadyCa
     private GoogleMap mMap;
     private SupportMapFragment mMapFragment;
     private AuthProvider mAuthProvider;
+    private LocationRequest mLocationRequest;
+    private FusedLocationProviderClient mFusedLocation;
+
+    private final static int LOCATION_REQUEST_CODE = 1;
+
+
+
+    //Call back para mandar la ubicacion cuando el usuario se mueva
+    LocationCallback mLocationCallback = new LocationCallback(){
+        @Override
+        public void onLocationResult(LocationResult locationResult){
+            for(Location location: locationResult.getLocations()){
+                if (getApplicationContext() != null){
+                  // Obtener la localizacion del usuario en tiempo real
+                    mMap.moveCamera(CameraUpdateFactory.newCameraPosition(
+                            new CameraPosition.Builder()
+                            .target(new LatLng(location.getLatitude(), location.getLongitude()))
+                            .zoom(15f)
+                            .build()
+                    ));
+
+                }
+            }
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +75,7 @@ public class MapDriverActivity extends AppCompatActivity implements OnMapReadyCa
         setContentView(R.layout.activity_map_driver);
         //Vamos a mostrar el toobar
         MyToolbar.show(this, "Conductor", false);
+        mFusedLocation = LocationServices.getFusedLocationProviderClient(this);
         //Instanciamos la variables
         //Nos marco error al acceder al metodo getSupportFragmentManager asi que tuvimos que castear
         //al tipo SupportMapFragment
@@ -45,7 +89,71 @@ public class MapDriverActivity extends AppCompatActivity implements OnMapReadyCa
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        //Instanceamos estas propiedades para obtener la ubicacion en tiempo real
+        mLocationRequest = new LocationRequest();
+        //Es la propiedad que define el rango de tiempo en el que se va ha estar actualizando la
+        //ubicacion del usuario en el mapa valores entre 1000 y 3000 milisegundos
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        //prioridad del gps para trabajar en la actualizacion de la ubicacion
+        //PARA QUE SE HAGA CON LA MAYOR PRECISION POSIBLE
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(5);
+        startLocation();
     }
+
+
+    //Para obtener los permisos sobre la ubicacion
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //condicional
+        if (requestCode == LOCATION_REQUEST_CODE){
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    mFusedLocation.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+                }
+            }
+        }
+    }
+
+    //Metodo para aescuchar el request de nuestra aplicacion
+    private void startLocation(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
+             mFusedLocation.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+            }else{
+             checkLocationPermissions();
+         }
+        }else{
+            mFusedLocation.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+        }
+    }
+
+
+    //Metodo para validar que pasaria si el usuario no acepta los permisos
+    private void checkLocationPermissions(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)){
+                new AlertDialog.Builder(this)
+                        .setTitle("Proporciona los permisos para continuar")
+                        .setMessage("Esta apaicacion requiere de los permisos de ubicacion para poder utilizarce")
+                        .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                //ESTA LINEA HABILITA LOS PERMISOS PARA UTILZAR LA UBICACION DEL CELULAR
+                                ActivityCompat.requestPermissions(MapDriverActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+                            }
+                        })
+                        .create()
+                        .show();
+                }else{
+                ActivityCompat.requestPermissions(MapDriverActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+                }
+            }
+        }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
